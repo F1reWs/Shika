2f548615bd3383ae991a41a44a9dda640e472d6e
F1reW <97834402+F1reWs@users.noreply.github.com>
2023-08-30 12:53:18 +0300

    улучшение дизайна

--- shika/modules/tester.py
+++ shika/modules/tester.py
@@ -38,104 +38,85 @@ class TesterMod(loader.Module):
 
         if not args or lvl < 0 or lvl > 60:
             return await utils.answer(
-                message, "❌ Вы не указали уровень или указали неверный уровень логов")
+                message, "<b><emoji id=5312526098750252863>❌</emoji> Вы не указали уровень или указали неверный уровень логов</b>")
 
         handler: CustomStreamHandler = log.handlers[1] # type: ignore
         logs = '\n'.join(str(error) for error in handler.logs).encode('utf-8')
         
         if not logs:
             return await utils.answer(
-                message, f"❕ Нет логов на уровне {lvl} ({logging.getLevelName(lvl)})")
+                message, f"<b><emoji id=5314302076317081739>❕</emoji> Нет логов на уровне {lvl} ({logging.getLevelName(lvl)})</b>")
 
         logs = io.BytesIO(logs)
         logs.name = "shika.log"
 
         return await message.reply_document(
             document=logs,
-            caption=f"📤 shika Логи с {lvl} ({logging.getLevelName(lvl)}) уровнем"
+            caption=f"<b><emoji id=5433614747381538714>📤</emoji> Shika Логи с {lvl} ({logging.getLevelName(lvl)}) уровнем</b>"
             )
     
     async def setprefix_cmd(self, app: Client, message: types.Message, args: str):
         """Изменить префикс, можно несколько штук разделённые пробелом. Использование: setprefix <префикс> [префикс, ...]"""
         if not (args := args.split()):
             return await utils.answer(
-                message, "❔ На какой префикс нужно изменить?")
+                message, "<b><emoji id=6334850502722848701>❔</emoji> На какой префикс нужно изменить?</b>")
 
         self.db.set("shika.loader", "prefixes", list(set(args)))
         prefixes = ", ".join(f"<code>{prefix}</code>" for prefix in args)
         return await utils.answer(
-            message, f"✅ Префикс был изменен на {prefixes}")
-
-    async def setlang_cmd(self, app: Client, message: types.Message, args: str):
-        """Изменить язык. Использование: setlang <язык>"""
-        args = args.split()
-        
-        language = args[0]
-        languages = list(map(lambda x: x.replace('.yml', ''), os.listdir('shika/langpacks')))
-        
-        if not args:
-            return await utils.answer(
-                message, "❔ На какой язык нужно изменить?")
-        
-        if language not in languages:
-            langs = ' '.join(languages)
-            return await utils.answer(
-                message, f'❌ Язык не найден. Доступные языки: <code>{langs}</code>')
-
-        self.db.set("shika.loader", "lang", language)
-        return await utils.answer(
-            message, f"✅ Язык был изменен на {language}")
+            message, f"<b><emoji id=6334758581832779720>✅</emoji> Префикс был изменен на {prefixes}</b>")
 
     async def addalias_cmd(self, app: Client, message: types.Message, args: str):
         """Добавить алиас. Использование: addalias <новый алиас> <команда>"""
+        prefix = self.db.get("shika.loader", "prefixes", ["."])[0]
         if not (args := args.lower().split(maxsplit=1)):
             return await utils.answer(
-                message, "❔ Какой алиас нужно добавить?")
+                message, "<b><emoji id=6334850502722848701>❔</emoji> Какой алиас нужно добавить?</b>")
 
         if len(args) != 2:
             return await utils.answer(
-                message, "❌ Неверно указаны аргументы."
-                        "✅ Правильно: addalias <новый алиас> <команда>"
+                message, "<b><emoji id=5312526098750252863>❌</emoji> Неверно указаны аргументы.</b>"
+                        f"<emoji id=6334758581832779720>✅</emoji><b> Правильно: <code>{prefix}addalias новый_алиас команда</code></b>"
             )
 
         aliases = self.all_modules.aliases
         if args[0] in aliases:
             return await utils.answer(
-                message, "❌ Такой алиас уже существует")
+                message, "<b><emoji id=5312526098750252863>❌</emoji> Такой алиас уже существует</b>")
 
         if not self.all_modules.command_handlers.get(args[1]):
             return await utils.answer(
-                message, "❌ Такой команды нет")
+                message, "<b><emoji id=5312526098750252863>❌</emoji> Такой команды нет</b>")
 
         aliases[args[0]] = args[1]
         self.db.set("shika.loader", "aliases", aliases)
 
         return await utils.answer(
-            message, f"✅ Алиас <code>{args[0]}</code> для команды <code>{args[1]}</code> был добавлен")
+            message, f"<emoji id=6334758581832779720>✅</emoji><b> Алиас <code>{args[0]}</code> для команды <code>{args[1]}</code> был добавлен</b>")
 
     async def delalias_cmd(self, app: Client, message: types.Message, args: str):
         """Удалить алиас. Использование: delalias <алиас>"""
         if not (args := args.lower()):
             return await utils.answer(
-                message, "❔ Какой алиас нужно удалить?")
+                message, "<emoji id=6334850502722848701>❔</emoji><b> Какой алиас нужно удалить?</b>")
 
         aliases = self.all_modules.aliases
         if args not in aliases:
             return await utils.answer(
-                message, "❌ Такого алиаса нет")
+                message, "<emoji id=5312526098750252863>❌</emoji><b> Такого алиаса нет</b>")
 
         del aliases[args]
         self.db.set("shika.loader", "aliases", aliases)
 
         return await utils.answer(
-            message, f"✅ Алиас <code>{args}</code> был удален")
+            message, f"<emoji id=6334758581832779720>✅</emoji><b> Алиас <code>{args}</code> был удален</b>")
 
     async def aliases_cmd(self, app: Client, message: types.Message):
         """Показать все алиасы"""
         aliases = self.all_modules.aliases
         if not aliases:
             return await utils.answer(
-                message, "Алиасов нет")
+                message, "<emoji id=5312526098750252863>❌</emoji><b>Алиасов нет</b>")
 
         return await utils.answer(
             message, "🗄 Список всех алиасов:\n" + "\n".join(
@@ -145,14 +126,14 @@ class TesterMod(loader.Module):
         )
 
     async def ping_cmd(self, app: Client, message: types.Message, args: str):
-        """🍵 команда для просмотра пинга."""
+        """Команда для показание пинга"""
         start = time.perf_counter_ns()
-        await utils.answer(message, "☕")
+        await utils.answer(message, "🌀")
         ping = round((time.perf_counter_ns() - start) / 10**6, 3)
         await utils.answer(
             message,
             f"""
-🕒 **Время отлика Telegram**: `{ping}ms`
+<emoji id=5445284980978621387>🚀</emoji> **Время отлика Telegram:** `{ping}ms`
             """
         )
 
