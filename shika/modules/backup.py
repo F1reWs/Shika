8cd5ea62e7c297608e50645679ad9997cb96fcb9
Firew <97834402+F1reWs@users.noreply.github.com>
2023-08-31 22:28:48 +0300

    Update backup.py

--- shika/modules/backup.py
+++ shika/modules/backup.py
@@ -63,7 +63,7 @@ class BackuperMod(loader.Module):
         msg = await message.edit(f"<b><emoji id=5328274090262275771>🕐</emoji> Обновляем базу данных...</b>")
         
         down = await reply.download(str(self.db.location))
-        shutil.copy2(str(down), "/shika/db.json")
+        shutil.copy2(str(down), "../shika/db.json")
         
         await msg.edit(f"<b><emoji id=5774134533590880843>🔄</emoji> База данных обновлена!</b>")
 
