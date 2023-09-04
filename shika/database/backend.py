faafbf9f40f07de26f318e0ef799276531fe7f1c
Firew <97834402+F1reWs@users.noreply.github.com>
2023-09-04 17:58:45 +0300

    Update backend.py

--- shika/database/backend.py
+++ shika/database/backend.py
@@ -28,26 +28,25 @@ class CloudDatabase:
         self._app = app
         self._me = me
         self.data_chat = None
-    
-     in future
-         asyncio.get_event_loop().create_task(
-             self.find_data_chat())
+
+        asyncio.get_event_loop().create_task(
+            self.find_data_chat())
 
     async def find_data_chat(self):
-         """Информация о чате с данными"""
-         if not self.data_chat:
-             chat = [
-                 dialog.chat async for dialog in self._app.get_dialogs()
-                 if dialog.chat.title == f"shika-{self._me.id}-data"
-                 and dialog.chat.type == "supergroup"
-             ]
+        """Информация о чате с данными"""
+        if not self.data_chat:
+            chat = [
+                dialog.chat async for dialog in self._app.iter_dialogs()
+                if dialog.chat.title == f"sh1t-{self._me.id}-data"
+                and dialog.chat.type == "supergroup"
+            ]
 
-             if not chat:
-                 self.data_chat = await self._app.create_supergroup(f"shika-{self._me.id}-data")
-             else:
-                 self.data_chat = chat[0]
+            if not chat:
+                self.data_chat = await self._app.create_supergroup(f"sh1t-{self._me.id}-data")
+            else:
+                self.data_chat = chat[0]
 
-         return self.data_chat
+        return self.data_chat
 
     async def save_data(self, message: Union[types.Message, str]):
         """Сохранить данные в чат"""
